e787b613a0808072f4827c8143b2c0d25ea72dcf
fscfelipe <felipesobreira.c@hotmail.com>
2017-05-30 16:33:59 -0300

    adicionado mapa

--- src/pages/mapa/mapa.ts
+++ src/pages/mapa/mapa.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
@@ -10,8 +10,28 @@ export class MapaPage {
   titulo = "Mapa";
   icone = 'navigation';
 
+  @ViewChild('map') mapElement;
+  map: any;
+
   constructor(public navCtrl: NavController) {
 
   }
+
+  ionViewDidLoad(){
+      this.initMap();
+  }
+
+  initMap(){
+      let latLng = new google.maps.LatLng(-3.72839, -38.5234);
+
+      let mapOptions = {
+        center: latLng,
+        zoom: 15,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      }
+
+      this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions)
+
+  } 
 
 }
