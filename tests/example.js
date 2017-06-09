f956e878f33db3f87e8666f48700cd0ab9c61e5b
fscfelipe <felipesobreira.c@hotmail.com>
2017-06-08 23:07:04 -0300

    adicionado framework de teste

---
+++ tests/example.js
@@ -0,0 +1,9 @@
+describe('example test', function() {
+  it('should be true', function() {
+    expect('foo').toBe('faoo');
+  });
+});
+
+//Que o teste acima da erro, olhe o Prompt de comando para ver o erro.
+//P.S.: Os erros são executados em realtime, então se você corrigir acima
+//a linha de comando já apresentará o resultado.
